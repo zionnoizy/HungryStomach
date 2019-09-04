6ad7f2efe5cb31ccdd8ad31a78aaf95eeb68979c
zion <zion411915@outlook.com>
2019-09-04 14:48:22 -0700

    init commit

--- app/src/main/java/com/example/hungrystomach/Upload_Activity.java
+++ app/src/main/java/com/example/hungrystomach/Upload_Activity.java
@@ -1,5 +1,6 @@
 package com.example.hungrystomach;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.media.MediaPlayer;
@@ -7,6 +8,7 @@ import android.net.Uri;
 import android.os.Bundle;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -35,6 +37,12 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import static java.security.AccessController.getContext;
 
 public class Upload_Activity extends AppCompatActivity{
     private static final int PICK_IMAGE_REQUEST = 1;
@@ -46,11 +54,14 @@ public class Upload_Activity extends AppCompatActivity{
     private ImageView img_view;
     private ProgressBar progress_bar;
     private Uri image_uri;
+    private StorageReference storage_ref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        storage_ref = FirebaseStorage.getInstance().getReference();
 
         btn_choose_image = findViewById(R.id.btn_choose_image);
         btn_upload_image = findViewById(R.id.btn_upload_image);
@@ -86,17 +97,62 @@ public class Upload_Activity extends AppCompatActivity{
     }
 
     /*
+    private void uploading_image(){
+        @Override
+        public void onClick(View view) {
+            if(image_uri!=null) {
+                final ProgressDialog dialog = new ProgressDialog(getContext()); //ProgressBar?
+                dialog.setTitle("Uploading Image.");
+                btn_choose_image.setVisibility(View.GONE);
+
+                StorageReference storage_ref = storage_ref.child(image_uri.getLastPathSegment());
+
+                storage_ref.putFile(image_uri).addOnSuccessListener(this, new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot task_snapshot) {
+                        Uri downloadUrl = task_snapshot.getDownloadUrl();
+                        String content = downloadUrl.toString();
+                        Log.e(TAG, "Image Uploaded");
+                        if (content.length() > 0) {
+                            Message msg = new Message();
+                            msg.text = content;
+                            FirebaeDatabase.getInstance().getReference().child("message/").push.setValue(msg);
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        dialog.dismiss();
+                        Log.e(TAG, "");
+
+                    }
+                }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                        double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
+                        progressDialog.setMessage("Uploading " + (int) progress + "%");
+                    }
+                });
+            }
+        }
+    }
+    */
+
+
+
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
+
+
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null){
             image_uri = data.getData();
 
             Picasso.with(this).load(image_uri).into(img_view);
-
         }
     }
-    /*
+
 
 
     /*
